d2b321c01e67958a8b1d4eb9e5469fb9be1d6dac
draguntsow <draguntsow@yahoo.com>
2022-05-06 15:14:53 +0300

    Update filterPSOutputForJSON.py

--- Powershell/filterPSOutputForJSON.py
+++ Powershell/filterPSOutputForJSON.py
@@ -1,3 +1,4 @@
 # Python snippet for clearing the output of Convert-ToJson from encoding rudiments
+# NOTE: that's a dumb way, better open the file with uncoding utf-16-le and later resave with normal one
 
 users = users.replace("яю", "").replace("\x00","")
